fd0cf63dd1f44b55c4443671f50c8876d9019e07
Tushar Sharma <43691776+TsBhardwaj@users.noreply.github.com>
2019-06-28 23:31:26 +0530

    Update Hashtable.py

--- Hashtable.py
+++ Hashtable.py
@@ -1,21 +1,22 @@
-import numpy as np
+import numpy as ny
 def hashTable(patch,Qangle,Qstrenth,Qcoherence):
-    [gx,gy] = np.gradient(patch)
-    G = np.matrix((gx.ravel(),gy.ravel())).T
+    #convert the matrix into 2-D array(convert the  value of matrix into patches and stores as array) ...
+    [gx,gy] = ny.gradient(patch)
+    G = ny.matrix((gx.ravel(),gy.ravel())).T
     x = G.T*G
-    [eigenvalues,eigenvectors] = np.linalg.eig(x)
+    [eigenvalues,eigenvectors] = ny.linalg.eig(x)
     #For angle
-    angle = np.math.atan2(eigenvectors[0,1],eigenvectors[0,0])
+    angle = ny.math.atan2(eigenvectors[0,1],eigenvectors[0,0])
     if angle<0:
-        angle += np.pi
+        angle += ny.pi
     #For strength
     strength = eigenvalues.max()/(eigenvalues.sum()+0.0001)
     #For coherence
-    lamda1 = np.math.sqrt(eigenvalues.max())
-    lamda2 = np.math.sqrt(eigenvalues.min())
-    coherence = np.abs((lamda1-lamda2)/(lamda1+lamda2+0.0001))
+    lamda1 = ny.math.sqrt(eigenvalues.max())
+    lamda2 = ny.math.sqrt(eigenvalues.min())
+    coherence = ny.abs((lamda1-lamda2)/(lamda1+lamda2+0.0001))
     #Quantization
-    angle = np.floor(angle/(np.pi/Qangle)-1)
-    strength = np.floor(strength/(1.0/Qstrenth)-1)
-    coherence = np.floor(coherence/(1.0/Qcoherence)-1)
+    angle = ny.floor(angle/(np.pi/Qangle)-1)
+    strength = ny.floor(strength/(1.0/Qstrenth)-1)
+    coherence = ny.floor(coherence/(1.0/Qcoherence)-1)
     return int(angle),int(strength),int(coherence)
