89949a6469cf0d75c03b1ab42e5a7a1920b92287
Gaurav Bholla <37663361+NinjaGaurav@users.noreply.github.com>
2019-10-17 12:46:35 +0530

    raisr NUMBA

---
+++ cgls.py
@@ -0,0 +1,17 @@
+import numpy as np
+import numba as nb
+
+@nb.jit(nopython=True, parallel=True)
+def cgls(A, b):
+    height, width = A.shape
+    x = np.zeros((height))
+    while(True):
+        sumA = A.sum()
+        if (sumA < 100):
+            break
+        if (np.linalg.det(A) < 1):
+            A = A + np.eye(height, width) * sumA * 0.000000005
+        else:
+            x = np.linalg.inv(A).dot(b)
+            break
+    return x
